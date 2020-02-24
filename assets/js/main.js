332353bef15d14d5e4f31bdb6ca8799c9bc89b65
Danielle Tait <Danielletait@hotmail.co.uk>
2020-02-24 12:18:21 +0000

    added js to maps 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,12 @@
-var marker = new google.maps.Marker({
+var map, places, infoWindow;
+var markers = [];
+var autocomplete;
+var countryRestrict = {'country': 'us'};
+var MARKER_PATH = 'assets/images/marker1.png'
+var hostnameRegexp = new RegExp('^https?://.+?/');
 
-    position: { lat: 7.477772, lng: -4.224721 },
-
-    map:map
-
-});
+var countries =  },
+  'uk': {
+    center: {lat: 54.8, lng: -4.6},
+    zoom: 5
+  }
