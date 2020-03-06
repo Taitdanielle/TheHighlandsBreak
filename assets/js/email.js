e110bafe5eebeba52176d479e6e9044a16342543
Danielle Tait <Danielletait@hotmail.co.uk>
2020-03-06 09:22:55 +0000

    emailjs

--- assets/js/email.js
+++ assets/js/email.js
@@ -40,7 +40,7 @@ function validateForm (){
     }
     //Once validation is complete emailjs is set to send the details
     //to an email address.
-    emailjs.sendForm ("gmail", "TheHighlandsBreak", {
+    emailjs.send("gmail", "TheHighlandsBreak", {
       "from_name": name,
       "from_email": email,
       "telephone": phone.value,
